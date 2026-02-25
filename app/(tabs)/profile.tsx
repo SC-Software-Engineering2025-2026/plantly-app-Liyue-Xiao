525f0c0c4d9d275b344f427a0249b97c65e55462
Chris Xiao <liyue.xiao@scsstudent.org>
2026-02-24 23:43:04 -0800

    feat: Complete video 4

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { theme } from "@/theme";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}> Open up App.tsx to start working</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colorWhite,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+  },
+});

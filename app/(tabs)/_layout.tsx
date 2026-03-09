59abe58dcf143d1bd01605a7f291ce41752f73f6
Chris Xiao <liyue.xiao@scsstudent.org>
2026-03-09 00:00:19 -0700

    feat:? I mean I didn't do anything really... Android and IOS do not work

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import { Redirect, Tabs, Link } from "expo-router";
+import { Redirect, Tabs, Link, SplashScreen } from "expo-router";
 import Entypo from "@expo/vector-icons/Entypo";
 import Feather from "@expo/vector-icons/Feather";
 import { theme } from "@/theme";
@@ -12,6 +12,7 @@ export default function Layout() {
     (state) => state.hasFinishedOnboarding,
   );
 
+  SplashScreen.hideAsync();
   if (!hasFinishedOnboarding) {
     return <Redirect href="/onboarding" />;
   }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
-import { Stack } from "expo-router";
+import { Stack, SplashScreen } from "expo-router";
 
 export default function Layout() {
+  SplashScreen.preventAutoHideAsync();
   return (
     <Stack>
       <Stack.Screen
